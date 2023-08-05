c6f02d1ee133887b828cbddf6c0efc296ff172f3
Cody Neal <codyrneal11@gmail.com>
2023-08-05 13:40:18 -0500

    General app structure added

--- AngularProj/src/app/app.component.ts
+++ AngularProj/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { WeatherViewComponent } from './weather-view/weather-view.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
   title = 'AngularProj';
+  @ViewChild(NavbarComponent) navbarChild: NavbarComponent | undefined;
+  @ViewChild(WeatherViewComponent) weatherViewChild: WeatherViewComponent | undefined;
+  @ViewChild(FooterComponent) footChild: FooterComponent | undefined;
 }
---
+++ AngularProj/src/app/weather-view/weather-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-weather-view',
+  templateUrl: './weather-view.component.html',
+  styleUrls: ['./weather-view.component.css']
+})
+export class WeatherViewComponent {
+
+}

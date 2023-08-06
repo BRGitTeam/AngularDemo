0c908ca74dbefb49168c883b16cf6624f229ee32
Cody Neal <codyrneal11@gmail.com>
2023-08-05 19:47:29 -0500

    Adds functionality to search for city and return weather data

--- AngularProj/src/app/app.module.ts
+++ AngularProj/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms'; 
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { WeatherViewComponent } from './weather-view/weather-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { WeatherViewComponent } from './weather-view/weather-view.component';
     WeatherViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularProj/src/app/weather-view/weather-view.component.ts
+++ AngularProj/src/app/weather-view/weather-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -6,6 +6,42 @@ import { Component } from '@angular/core';
   templateUrl: './weather-view.component.html',
   styleUrls: ['./weather-view.component.css']
 })
-export class WeatherViewComponent {
 
+
+export class WeatherViewComponent{
+  public searchText: string = '';
+  public currentTemp: Number | undefined;
+  public weatherCondition: string | undefined;
+  public showTemp: Boolean = false;
+  public timeZone: string | undefined;
+  public img: any | undefined;
+  public city: string | undefined;
+  public state: string | undefined;
+
+
+   onCitySubmit = async () => {
+    console.log(this.searchText)
+    let url = 'https://api.weatherapi.com/v1/current.json'
+    let API_KEY = '8eb1ea8eac8b4bbfb51223921230508'
+ 
+    try{
+      this.showTemp = true;
+      let jsonData = await fetch(url +'?key=' + API_KEY + '&q=' + this.searchText)
+      let weatherData = await jsonData.json();
+      this.currentTemp = weatherData.current.temp_f;
+      this.timeZone = weatherData.location.tz_id;
+      this.weatherCondition = weatherData.current.condition.text;
+      this.img = weatherData.current.condition.icon;
+      this.city = weatherData.location.name;
+      this.state = weatherData.location.region;
+    }catch(e){
+      throw e;
+      console.log(e)
+    }
+
+    this.searchText = ''; //Setting input field back to original state.
+
+
+  
+  }
 }
